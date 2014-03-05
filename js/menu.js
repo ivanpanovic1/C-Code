e2d619522ecacdce565a08fea27e19f1622becaf
zoran.knezevic <knezeviczoran@gmail.com>
2014-03-05 11:16:57 +0100

    test
    test

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+$(function() {
+			var pull 		= $('#pull');
+				menu 		= $('nav ul');
+				menuHeight	= menu.height();
+
+			$(pull).on('click', function(e) {
+				e.preventDefault();
+				menu.slideToggle();
+				$('#pull').toggleClass('expand');
+			});
+
+			$(window).resize(function(){
+        		var w = $(window).width();
+        		if(w > 320 && menu.is(':hidden')) {
+        			menu.removeAttr('style');
+        		}
+    		});
+		});
---
+++ js/scrolltop.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+$(function() {
+ 
+	$('#top').click(function() {
+		$('body,html').animate({scrollTop:0},1500);
+	});	
+});
